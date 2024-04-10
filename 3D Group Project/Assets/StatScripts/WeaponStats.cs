0aa1f67c26c29aee174e8b4687806f8cda85dd39
DraconicLegacy <147756467+DraconicLegacy@users.noreply.github.com>
2024-04-10 12:05:35 -0700

    Updated WeaponStat Script

--- 3D Group Project/Assets/StatScripts/WeaponStats.cs
+++ 3D Group Project/Assets/StatScripts/WeaponStats.cs
@@ -9,18 +9,21 @@ public class WeaponStats : MonoBehaviour
     public float WepMainStat = 1;
     public float WepStamina = 1;
     public int WepReqLvl = 1;
-    int WepMaxReq = 1;
+    [SerializeField] int WepMaxReq = 1;
     [SerializeField] float WepILvl = 1;
 
     void Awake()
     {
-        
         WepReqLvl = Random.Range(1, WepMaxReq);
-        WepILvl = 20 * WepReqLvl + (3 * WepRarity);
-        WepDamage = WepILvl * WepReqLvl;
-        WepMainStat = WepDamage + (WepILvl * 2.5f);
+        WepILvl = 20 * WepReqLvl + (3 * WepRarity) / 10;
+        WepDamage = WepILvl * WepReqLvl / 30;
+        WepMainStat = WepDamage + (WepILvl * 2.5f) / 10;
         WepStamina = WepMainStat * 1.15f;
 
+        WepILvl = (int) WepILvl;
+        WepDamage = (int) WepDamage;
+        WepMainStat = (int) WepMainStat;
+        WepStamina = (int) WepStamina;
     }
 
 }
