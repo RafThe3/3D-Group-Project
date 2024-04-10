b0f17ba68677c45c261470bc94701b1d3e74eabe
DraconicLegacy <147756467+DraconicLegacy@users.noreply.github.com>
2024-04-10 12:08:01 -0700

    Fixxed Zero Damage bug

--- 3D Group Project/Assets/StatScripts/WeaponStats.cs
+++ 3D Group Project/Assets/StatScripts/WeaponStats.cs
@@ -9,6 +9,7 @@ public class WeaponStats : MonoBehaviour
     public float WepMainStat = 1;
     public float WepStamina = 1;
     public int WepReqLvl = 1;
+    [SerializeField] int WepMinDamage = 5;
     [SerializeField] int WepMaxReq = 1;
     [SerializeField] float WepILvl = 1;
 
@@ -16,7 +17,7 @@ public class WeaponStats : MonoBehaviour
     {
         WepReqLvl = Random.Range(1, WepMaxReq);
         WepILvl = 20 * WepReqLvl + (3 * WepRarity) / 10;
-        WepDamage = WepILvl * WepReqLvl / 30;
+        WepDamage = WepILvl * WepReqLvl / 30 + WepMinDamage;
         WepMainStat = WepDamage + (WepILvl * 2.5f) / 10;
         WepStamina = WepMainStat * 1.15f;
 
