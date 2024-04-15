db16e10777c4e387d50f23e64716e7bdf2d0f81f
DraconicLegacy <147756467+DraconicLegacy@users.noreply.github.com>
2024-04-15 11:40:34 -0700

    Added Inventory system

---
+++ 3D Group Project/Assets/InventoryCode/InventorySlot.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class InventorySlot : MonoBehaviour, IPointerClickHandler
+{
+    public InventoryItems myItem { get; set; }
+    public SlotTag myTag;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if(eventData.button == PointerEventData.InputButton.Left)
+        {
+            if(Inventory.carriedItem == null) return;
+            if(myTag != SlotTag.None && Inventory.carriedItem.myItem.myTag != myTag) return;
+            SetItem(Inventory.carriedItem);
+        }
+    }
+
+    public void SetItem(InventoryItems item)
+    {
+        Inventory.carriedItem = null;
+
+        item.activeSlot.myItem = null;
+
+        myItem = item;
+        myItem.activeSlot = this;
+        myItem.transform.SetParent(transform);
+        myItem.canvasGroup.blocksRaycasts = true;
+
+        if(myTag != SlotTag.None)
+        {
+            Inventory.PlayerInventory.EquipEquipment(myTag, myItem);
+        }
+    }
+}
