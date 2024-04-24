24a822be17f135264b1661843b108f3e9b4c2796
LEDstrike <24.gabriel.mitchell@KSD.org>
2024-04-24 14:01:11 -0700

    stuff

---
+++ 3D Group Project/Assets/PlayerSpawn.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawn : MonoBehaviour
+{
+    [SerializeField] private GameObject mage, ranger, warrior;
+    [SerializeField] private Transform spawnPoint;
+
+    private void Start()
+    {
+        Time.timeScale = 1;
+        SpawnPlayer();
+    }
+
+    private void SpawnPlayer()
+    {
+        switch (PlayerPrefs.GetInt("PlayerToSpawn"))
+        {
+            case 1:
+                Instantiate(mage, spawnPoint.position, Quaternion.identity);
+                break;
+
+            case 2:
+                Instantiate(ranger, spawnPoint.position, Quaternion.identity);
+                break;
+
+            case 3:
+                Instantiate(warrior, spawnPoint.position, Quaternion.identity);
+                break;
+
+            default:
+                break;
+        }
+    }
+}
