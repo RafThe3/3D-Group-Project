1395971286ab0bb2e3f5c1d9f72063321c19ef8f
Raf <25.rafael.gutierrez@ksd.org>
2024-05-03 11:50:25 -0700

    modified NPC script

--- 3D Group Project/Assets/Scripts/NPC.cs
+++ 3D Group Project/Assets/Scripts/NPC.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using TMPro;
 
 public class NPC : MonoBehaviour
 {
@@ -9,10 +10,14 @@ public class NPC : MonoBehaviour
     [SerializeField] private AudioClip walkSFX;
     [SerializeField] private AudioSource audioSource;
     [Min(1), SerializeField] private float audioSpeed = 1;
+    [SerializeField] private TextMeshProUGUI dialogueText;
+    [SerializeField] private TextMeshProUGUI interactText;
+    [SerializeField] private string[] dialogue;
 
     private NavMeshAgent agent;
     private Animator animator;
     private float audioLength = 0;
+    private RaycastHit hit;
 
     private void Awake()
     {
@@ -20,11 +25,27 @@ public class NPC : MonoBehaviour
         animator = GetComponent<Animator>();
     }
 
+    private void Start()
+    {
+        dialogueText.enabled = false;
+        interactText.enabled = false;
+    }
+
     private void Update()
     {
         audioLength += Time.deltaTime;
 
         MoveNPC();
+
+        Ray ray = new(Camera.main.transform.position, Camera.main.transform.forward);
+
+        if (Physics.Raycast(ray, out hit, 5) && hit.collider.CompareTag("NPC") && Input.GetKeyDown(KeyCode.E))
+        {
+            dialogueText.enabled = true;
+            dialogueText.text = "Some dialogue lke fsdhfs";
+        }
+
+        interactText.enabled = hit.collider.CompareTag("NPC");
     }
 
     private void MoveNPC()
